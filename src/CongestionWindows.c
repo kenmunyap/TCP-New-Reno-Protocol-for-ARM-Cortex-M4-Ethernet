4cde4b3d635bb7c730d97bba2e17cc97824cb983
9drashijin <9drashijin@live.com>
2015-08-07 18:41:36 +0800

    fix the congestion window checking and return the available size
    Signed-off-by: 9drashijin <9drashijin@live.com>

--- src/CongestionWindows.c
+++ src/CongestionWindows.c
@@ -4,6 +4,9 @@
 #include "CongestionWindows.h"
 #include "Packet.h"
 
+uint8_t Buffer[1000];
+#define Val 'A'
+
 uint32_t cwndGetBeginningOffsets(Cwnd *cwnd){
   return cwnd->offset;
 }
@@ -18,16 +21,24 @@ uint32_t cwndIncrementWindows(Cwnd *cwnd, uint32_t size){
 }
 
 uint32_t cwndGetDataBlocks(Cwnd *cwnd, uint32_t offset, uint32_t requestedSize, uint8_t **block){
+  uint32_t i;
+  uint32_t counter = 0;
   uint32_t availableSize;
   uint32_t Offset = cwnd->offset;
   uint32_t Size = cwnd->size;
-  uint32_t *Block = &Size;
-  // uint32_t *Blocksize = (uint32_t*)(&(*block));
-     
-  if (((Size - requestedSize) < Size) && (Offset - offset == 0)){
   
-    availableSize = requestedSize;
-    
+  *block = Buffer;
+  
+  if (((Size - requestedSize) < Size) && (Offset - offset == 0)){
+    for(i = 0 ; i < requestedSize ; i++){
+      Buffer[i] = Val+1;
+      if(Buffer[i]){
+        counter++;
+      }
+      if(counter == requestedSize){
+        availableSize = requestedSize;
+      }
+    }
     return availableSize;
   }
   return 0;
